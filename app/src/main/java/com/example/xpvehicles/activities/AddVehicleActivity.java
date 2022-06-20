19a5f01105d1eaac03fa5dc8f19c79a99a8e08a9
Joshua Bello <joshuabello@fb.com>
2022-06-20 13:44:34 -0700

    Create floating action bar for adding a vehicle and intent to transition to AddVehicleActivity

---
+++ app/src/main/java/com/example/xpvehicles/activities/AddVehicleActivity.java
@@ -0,0 +1,16 @@
+package com.example.xpvehicles.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.xpvehicles.R;
+
+public class AddVehicleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_vehicle);
+    }
+}
--- app/src/main/java/com/example/xpvehicles/fragments/ExploreFragment.java
+++ app/src/main/java/com/example/xpvehicles/fragments/ExploreFragment.java
@@ -1,11 +1,14 @@
 package com.example.xpvehicles.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.example.xpvehicles.R;
+import com.example.xpvehicles.activities.AddVehicleActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class ExploreFragment extends Fragment {
@@ -16,11 +19,17 @@ public class ExploreFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_explore, parent, false);
     }
 
-    // This event is triggered soon after onCreateView().
-    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        // Setup any handles to view objects here
-        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+        FloatingActionButton fabAddVehicle = view.findViewById(R.id.fabAddVehicle);
+        addVehicle(fabAddVehicle);
+    }
+
+    private void addVehicle(FloatingActionButton fabAddVehicle) {
+        fabAddVehicle.setOnClickListener(v -> {
+            Intent i = new Intent(getContext(), AddVehicleActivity.class);
+            startActivity(i);
+        });
+
     }
 }
