a5fa8fb6766a2eeb2d75052e68ff3664b3160050
Joshua Bello <joshuabello@fb.com>
2022-06-21 11:18:55 -0700

    Vehicle card ui

--- app/src/main/java/com/example/xpvehicles/activities/AddVehicleActivity.java
+++ app/src/main/java/com/example/xpvehicles/activities/AddVehicleActivity.java
@@ -1,20 +1,32 @@
 package com.example.xpvehicles.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.RequestParams;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.codepath.asynchttpclient.callback.TextHttpResponseHandler;
 import com.example.xpvehicles.R;
 import com.example.xpvehicles.models.Vehicle;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.parse.Parse;
 import com.parse.ParseUser;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.Headers;
+
 public class AddVehicleActivity extends AppCompatActivity {
 
+    private static final String TAG = "Add_Vehicle_Activity";
     private MaterialToolbar topAppBar;
     private EditText edtVehicleName;
     private EditText edtDescription;
@@ -56,7 +68,11 @@ public class AddVehicleActivity extends AppCompatActivity {
         btnAddVehicle.setOnClickListener(v -> {
             String vehicleName = edtVehicleName.getText().toString();
             String description = edtDescription.getText().toString();
-            Double dailyPrice = Double.valueOf(edtDailyPrice.getText().toString());
+            String streetAddress = edtStreetAddress.getText().toString();
+            String city = edtCity.getText().toString();
+            String state = edtState.getText().toString();
+            String zipCode = edtZipCode.getText().toString();
+//            Double dailyPrice = Double.valueOf(edtDailyPrice.getText().toString());
             ParseUser owner = ParseUser.getCurrentUser();
 
 
@@ -64,13 +80,36 @@ public class AddVehicleActivity extends AppCompatActivity {
             vehicle.setOwner(owner);
             vehicle.setVehicleName(vehicleName);
             vehicle.setDescription(description);
-            vehicle.setDailyPrice(dailyPrice);
-            getGeoLocation();
+//            vehicle.setDailyPrice(dailyPrice);
+            getGeocodedLocation(streetAddress, city, state);
         });
 
     }
 
-    private void getGeoLocation() {
+    private void getGeocodedLocation(String streetAddress, String city, String state) {
+        String base_url = "https://maps.googleapis.com/maps/api/geocode/json?";
+        String address = streetAddress + " " + city + " " + state;
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("key", R.string.API_KEY);
+        params.put("address", address);
+        client.get(base_url, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONObject test = jsonObject.getJSONObject("results");
+                } catch (JSONException e) {
+                    Log.e(TAG, "hit json exception when getting the latitude and longitude");
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.e(TAG, "Error sending the JSON request", throwable);
+            }
+        });
+
 
     }
 
